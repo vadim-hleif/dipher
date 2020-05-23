df1dd9b08eee429be5c64639d4d807e2cb97a689
vadim-hleif <vadzim.hleif@gmail.com>
2020-05-24 00:17:43 +0500

    chore: bump version

--- cmd/version.go
+++ cmd/version.go
@@ -9,6 +9,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of dipher",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.0.2")
+		fmt.Println("0.1")
 	},
 }
